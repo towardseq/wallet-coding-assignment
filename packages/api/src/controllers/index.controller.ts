f96584b01b75fecc9cf59930327be4fbf2fba8d6
Zach Goldberg <zach@equi.com>
2023-02-27 14:25:14 -0800

    chore: use the service to avoid building a prismaclient every req

--- packages/api/src/controllers/index.controller.ts
+++ packages/api/src/controllers/index.controller.ts
@@ -1,7 +1,9 @@
-import { PrismaClient } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
+import userService from '@services/users.service';
 
 class IndexController {
+  public userService = new userService();
+
   public index = (req: Request, res: Response, next: NextFunction): void => {
     try {
       res.sendStatus(200);
@@ -12,9 +14,7 @@ class IndexController {
 
   public healthcheck = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const prisma = new PrismaClient();
-      const numUsers = (await prisma.user.findMany()).length;
-      console.log(`Have ${numUsers} users`);
+      const numUsers = (await this.userService.findAllUser()).length;
       res.sendStatus(200);
     } catch (error) {
       next(error);
