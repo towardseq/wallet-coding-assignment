2cb173b44403a523182b1ea0e50da429d1cf36f9
Zach Goldberg <zach@equi.com>
2023-02-27 14:19:43 -0800

    feat: use psql, get it working w/docker

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,16 @@ FROM node:18 AS api
 
 WORKDIR /usr/src/app/packages/api
 
-COPY package*.json ./
+COPY packages/api ./
+
+RUN npm i
+RUN npm run prisma:generate
 
 FROM node:18 AS app
 
 WORKDIR /usr/src/app/packages/webapp
 
 COPY package*.json ./
+
+FROM postgres:13.7-alpine as db
+COPY ./packages/database/init/init-user-db.sh /docker-entrypoint-initdb.d/init-user-db.sh
--- packages/api/src/controllers/index.controller.ts
+++ packages/api/src/controllers/index.controller.ts
@@ -1,8 +1,20 @@
+import { PrismaClient } from '@prisma/client';
 import { NextFunction, Request, Response } from 'express';
 
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction): void => {
     try {
+      res.sendStatus(200);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public healthcheck = async (req: Request, res: Response, next: NextFunction): void => {
+    try {
+      const prisma = new PrismaClient();
+      const numUsers = (await prisma.user.findMany()).length;
+      console.log(`Have ${numUsers} users`);
       res.sendStatus(200);
     } catch (error) {
       next(error);
